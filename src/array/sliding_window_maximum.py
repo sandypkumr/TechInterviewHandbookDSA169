67a6595cea58ee580fea99b959a70e774fa45c07
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2024-12-12 07:57:08 +0530

    sliding window maximum

---
+++ src/array/sliding_window_maximum.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums:
+            return []
+        if k == 1:
+            return nums
+        n = len(nums)
+        left = [0] * n
+        left[0] = nums[0]
+        right = [0] * n
+        right[n - 1] = nums[n - 1]
+        for i in range(1, n):
+            if i % k == 0:
+                left[i] = nums[i]
+            else:
+                left[i] = max(left[i - 1], nums[i])
+            j = n - i - 1
+            if (j + 1) % k == 0:
+                right[j] = nums[j]
+            else:
+                right[j] = max(right[j + 1], nums[j])
+        res = []
+        for i in range(n - k + 1):
+            res.append(max(left[i + k - 1], right[i]))
+        return res
