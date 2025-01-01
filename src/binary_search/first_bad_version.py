590bd8e8ea06585eb836ae6870dac71419cdf6e5
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-01-01 11:36:53 +0530

    first bad version

---
+++ src/binary_search/first_bad_version.py
@@ -0,0 +1,14 @@
+def isBadVersion(ver):
+    pass
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
