7181c9bddd678ef1c81e17f82b7b483e343e5852
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-01-13 18:45:29 +0530

    first missing positive

---
+++ src/hash_table/first_missing_positive.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        for i in range(1, len(nums) + 2):
+            if i not in nums:
+                return i
