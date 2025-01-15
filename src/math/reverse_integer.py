467cd7493c56e4538820914fb1c40dd11de6f606
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-01-15 17:03:15 +0530

    reverse integer

---
+++ src/math/reverse_integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x == 0:
+            return 0
+        sign = 1 if x > 0 else -1
+        x = abs(x)
+        res = 0
+        while x:
+            res = res * 10 + x % 10
+            x //= 10
+        res *= sign
+        if res < -2 ** 31 or res > 2 ** 31 - 1:
+            return 0
+        return res
