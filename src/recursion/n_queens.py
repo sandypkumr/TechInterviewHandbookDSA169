b00dbe7757adc7743651ad88097d200c6f85e5f2
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-03-06 17:34:57 +0530

    n queens

--- src/recursion/n_queens.py
+++ src/recursion/n_queens.py
@@ -3,41 +3,29 @@ from typing import List
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def is_not_under_attack(row, col):
-            return not (cols[col] + hill[row - col] + dale[row + col])
-
-        def place_queen(row, col):
-            queens.add((row, col))
-            cols[col] = 1
-            hill[row - col] = 1
-            dale[row + col] = 1
-
-        def remove_queen(row, col):
-            queens.remove((row, col))
-            cols[col] = 0
-            hill[row - col] = 0
-            dale[row + col] = 0
-
-        def add_solution():
-            solution = []
-            for _, col in sorted(queens):
-                solution.append('.' * col + 'Q' + '.' * (n - col - 1))
-            output.append(solution)
-
-        def backtrack(row):
+        def backtrack(row, cols, diags, anti_diags, board):
+            if row == n:
+                result.append(["".join(row) for row in board])
+                return
             for col in range(n):
-                if is_not_under_attack(row, col):
-                    place_queen(row, col)
-                    if row + 1 == n:
-                        add_solution()
-                    else:
-                        backtrack(row + 1)
-                    remove_queen(row, col)
-
-        cols = [0] * n
-        hill = [0] * (2 * n - 1)
-        dale = [0] * (2 * n - 1)
-        queens = set()
-        output = []
-        backtrack(0)
-        return output
+                diag = row - col
+                anti_diag = row + col
+                if col in cols or diag in diags or anti_diag in anti_diags:
+                    continue
+                board[row][col] = "Q"
+                cols.add(col)
+                diags.add(diag)
+                anti_diags.add(anti_diag)
+                backtrack(row + 1, cols, diags, anti_diags, board)
+                board[row][col] = "."
+                cols.remove(col)
+                diags.remove(diag)
+                anti_diags.remove(anti_diag)
+        if n == 1:
+            return [["Q"]]
+        if n < 4:
+            return []
+        result = []
+        board = [["." for _ in range(n)] for _ in range(n)]
+        backtrack(0, set(), set(), set(), board)
+        return result
