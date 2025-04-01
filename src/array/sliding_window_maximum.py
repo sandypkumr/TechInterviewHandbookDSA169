5b870190b124efcee2768fa7ce32288ebbcb3252
sandeepkumar.pabba <sandeepkumar.pabba@servicenow.com>
2025-04-01 13:18:55 +0530

    sliding window maximum

--- src/array/sliding_window_maximum.py
+++ src/array/sliding_window_maximum.py
@@ -1,28 +1,23 @@
+from collections import deque
 from typing import List
 
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        if not nums:
-            return []
         if k == 1:
             return nums
         n = len(nums)
-        left = [0] * n
-        left[0] = nums[0]
-        right = [0] * n
-        right[n - 1] = nums[n - 1]
-        for i in range(1, n):
-            if i % k == 0:
-                left[i] = nums[i]
-            else:
-                left[i] = max(left[i - 1], nums[i])
-            j = n - i - 1
-            if (j + 1) % k == 0:
-                right[j] = nums[j]
-            else:
-                right[j] = max(right[j + 1], nums[j])
         res = []
-        for i in range(n - k + 1):
-            res.append(max(left[i + k - 1], right[i]))
+        max_deque = deque()
+        for i in range(n):
+            # Remove elements not in the window
+            if max_deque and max_deque[0] < i - k + 1:
+                max_deque.popleft()
+            # Remove smaller elements in the window
+            while max_deque and nums[max_deque[-1]] <= nums[i]:
+                max_deque.pop()
+            max_deque.append(i)
+            # Append the maximum element of the current window
+            if i >= k - 1:
+                res.append(nums[max_deque[0]])
         return res
